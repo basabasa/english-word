fd6a8a043a708f0784ef49549bfd8d203478ad0a
basabasa <ryuryun2010@yahoo.co.jp>
2014-06-11 21:49:59 +0900

    create

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+var questions = map[int]string{}
+var answers = map[int]string{}
+
+func main() {
+	printLabel("Game start...")
+
+	// initialize
+	rand.Seed(time.Now().Unix())
+	prepareQuestions()
+
+	// game start
+	game()
+}
+
+func prepareQuestions() {
+	fp, err := os.Open("questions.txt")
+	if err != nil {
+		panic("Can not open the file of questions")
+	}
+	defer fp.Close()
+
+	reader := bufio.NewReaderSize(fp, 4096)
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic("Read error")
+		}
+		strs := strings.Split(string(line), ":")
+		if len(strs) == 2 {
+			answers[len(answers)] = strs[0]
+			questions[len(questions)] = strs[1]
+		}
+	}
+}
+
+func game() {
+	var r int = rand.Intn(len(questions))
+	var question string = questions[r]
+	var answer string = answers[r]
+
+	fmt.Printf("%s", question)
+	fmt.Println("")
+
+	var input string = scan()
+	input = strings.Trim(input, "\r\n")
+	input = strings.Trim(input, "\n")
+
+	fmt.Println("")
+	if input == answer {
+		fmt.Println("Success!!")
+	} else {
+		fmt.Println("failure…")
+		fmt.Printf("Answer : %s", answer)
+		fmt.Println("")
+	}
+	fmt.Println("")
+	printLabel("Next question")
+
+	game()
+}
+
+func scan() (input string) {
+	reader := bufio.NewReader(os.Stdin)
+	input, _ = reader.ReadString('\n')
+	return
+}
+
+func printLabel(label string) {
+	fmt.Printf("** %s", label)
+	fmt.Println("")
+	fmt.Println("************************")
+}
